977b97070b0652a631f0af881c742df4461808cc
Karl <karl_dilma@hotmail.com>
2024-05-15 18:17:30 +1000

    feat: select role functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+
+import { RadioGroup, RadioGroupItem, RadioOption } from "./components";
+
 function App(): JSX.Element {
+  const [role, setRole] = useState<string>("admin");
+
+  const onOptionChange = (roleOption: React.ChangeEvent<HTMLInputElement>) => {
+    setRole(roleOption.target.value);
+  };
+
+  const roles: RadioOption[] = [
+    { value: "admin", name: "Admin" },
+    { value: "manager", name: "Manager" },
+  ];
   return (
-    <div className="flex place-items-center min-w-[320px] min-h-screen">
-      <div className="w-screen p-8">
-        <h1>Customer Selection</h1>
+    <div className="flex min-w-[320px] min-h-screen">
+      <div className="w-screen p-12">
+        <h2 className="mb-6">User Types</h2>
+        <RadioGroup>
+          {roles.map(({ value, name }, i) => {
+            return (
+              <RadioGroupItem
+                key={i}
+                id={`radio-${value}-${i}`}
+                value={value}
+                {...(role === value && { checked: true })}
+                defaultChecked={role === value}
+                onChange={onOptionChange}
+              >
+                {name}
+              </RadioGroupItem>
+            );
+          })}
+        </RadioGroup>
       </div>
     </div>
   );
