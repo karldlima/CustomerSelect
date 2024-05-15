2244f7705381a0c323835ceea443a58deaba7a27
Karl <karl_dilma@hotmail.com>
2024-05-15 15:28:07 +1000

    feat: removing css files for tw

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
-
 function App(): JSX.Element {
   return (
-    <>
-      <h1>Customer Selection</h1>
-    </>
+    <div className="flex place-items-center min-w-[320px] min-h-screen">
+      <div className="w-screen p-8">
+        <h1>Customer Selection</h1>
+      </div>
+    </div>
   );
 }
 
