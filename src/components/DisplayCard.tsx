906e0b06873126129db155b65ceabceb7901aae7
Karl D'Lima <karl_dilma@hotmail.com>
2024-05-15 19:39:13 +1000

    DisplayCard Component (#3)
    * feat: displaycard component
    
    * feat: header styling tw

---
+++ src/components/DisplayCard.tsx
@@ -0,0 +1,21 @@
+interface DisplayCardProps {
+  name: string;
+  role: string;
+}
+
+export const DisplayCard = ({ name, role }: DisplayCardProps): JSX.Element => {
+  return (
+    <div className="w-full">
+      <div className="flex gap-4">
+        <div className="text-blue-600 bg-blue-100 w-10 h-10 rounded-md flex justify-center items-center leading-5">
+          {name[0]}
+        </div>
+        <div>
+          <h4>{name}</h4>
+          <h5>{role}</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+DisplayCard.displayName = "DisplayCard";
