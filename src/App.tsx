ad5f3973b5d02562440357c53b39a0bfb4f207b7
Karl D'Lima <karl_dilma@hotmail.com>
2024-05-15 18:19:19 +1000

    RadioGroup and Selecting Role Functionality (#2)
    * feat: removing css files for tw
    
    * build: clsx lib
    
    * feat: heading tw styles
    
    * feat: radiogroup component
    
    * feat: select role functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import "./App.css";
+import { useState } from "react";
+
+import { RadioGroup, RadioGroupItem, RadioOption } from "./components";
 
 function App(): JSX.Element {
+  const [role, setRole] = useState<string>("admin");
+
+  const onOptionChange = (roleOption: React.ChangeEvent<HTMLInputElement>) => {
+    setRole(roleOption.target.value);
+  };
+
+  const roles: RadioOption[] = [
+    { value: "admin", name: "Admin" },
+    { value: "manager", name: "Manager" },
+  ];
   return (
-    <>
-      <h1>Customer Selection</h1>
-    </>
+    <div className="flex min-w-[320px] min-h-screen">
+      <div className="w-screen p-12">
+        <h2 className="mb-6">User Types</h2>
+        <RadioGroup>
+          {roles.map(({ value, name }, i) => {
+            return (
+              <RadioGroupItem
+                key={i}
+                id={`radio-${value}-${i}`}
+                value={value}
+                {...(role === value && { checked: true })}
+                defaultChecked={role === value}
+                onChange={onOptionChange}
+              >
+                {name}
+              </RadioGroupItem>
+            );
+          })}
+        </RadioGroup>
+      </div>
+    </div>
   );
 }
 
